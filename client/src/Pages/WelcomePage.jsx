1667c20797284f1ba52264dc9a2221a12808579c
Jade Jolivet <jade.jolivet@gmail.com>
2024-05-16 11:57:41 +0200

    WelcomePage structure

--- client/src/Pages/WelcomePage.jsx
+++ client/src/Pages/WelcomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function WelcomePage() {
   return (
-    <div>WelcomePage</div>
-  )
+    <section>
+      <h2>Welcome on YES,MAN !</h2>
+      <p>Entrez dans l'univers de Yes,Man! Votre guide d'aventure par excellence, 
+        Que vous soyez un citadin curieux, un amoureux de la nature ou un passionné de sport, 
+        Yes,Man! vous propose une expérience unique. Après avoir choisi vos catégories préférées, 
+        laissez-vous surprendre par le lancer de dés qui vous mènera vers des aventures inattendues et palpitantes. 
+        Préparez-vous à dire oui à chaque nouvelle expérience, avec Yes,Man! comme complice de vos escapades les plus mémorables !</p>
+      <button type="Button" onClick={handleNextPage}>Start</button>
+    </section>
+  );
 }
 
-export default WelcomePage
+function handleNextPage() {
+  // Ajoutez ici le code pour la navigation vers la page suivante
+}
+
+export default WelcomePage;
