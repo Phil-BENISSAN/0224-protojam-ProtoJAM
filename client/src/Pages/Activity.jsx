79a83bbddfaf84bb0ddc315d2ba79cfddd2d1ed4
AXEL JANIAK <axeljpro@gmail.com>
2024-05-17 15:31:32 +0200

    lolololololo som eburegr

--- client/src/Pages/Activity.jsx
+++ client/src/Pages/Activity.jsx
@@ -1,18 +1,20 @@
-import Cards from '../components/Cards'
-import SearchBar from '../components/SearchBar';
-import data from '../data.json'
+import Cards from "../components/Cards";
+import SearchBar from "../components/SearchBar";
+import data from "../data.json";
 
 function Activity() {
-    const activities = data.activité || [];
-  
-    return ( <>
-      <section className='resultContainer'>
+  const activities = data.activité || [];
+
+  return (
+    <>
+      <section className="resultContainer">
+        <h2 className="ouaipouaip">Résultats aléatoire</h2>
         {activities.map((item) => (
           <Cards item={item} key={item.id} />
         ))}
       </section>
-      </>
-    );
-  }
-  
-  export default Activity;
+    </>
+  );
+}
+
+export default Activity;
